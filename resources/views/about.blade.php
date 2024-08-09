bd266e851e1e2d727af58f84a20e17cf474c8a7d
Abdelali Laaguidi <abdelali.laaguidi.17@gmail.com>
2024-08-09 20:47:01 +0100

    adding a nav link component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
-    <h1>About</h1>
+  <x-slot:heading>
+    About
+  </x-slot:heading>
+  <h1>Welcome to the About Page</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
-  <h1>Homepage</h1>
+  <x-slot:heading>
+    Homepage
+  </x-slot:heading>
+  <h1>Welcome to the Homepage</h1>
 </x-layout>
