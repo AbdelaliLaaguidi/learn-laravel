bd492c3046b5402c5240cdaf3e55083e9df89fc2
Abdelali Laaguidi <abdelali.laaguidi.17@gmail.com>
2024-08-09 21:51:05 +0100

    adding single job page

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,50 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-  return view('about');
+Route::get('/jobs', function () {
+  return view('jobs', [
+    'jobs' => [
+      [
+        'id' => 1,
+        'title' => 'Programmer',
+        'salary' => '$10.000',
+      ],
+      [
+        'id' => 2,
+        'title' => 'Designer',
+        'salary' => '$30.000',
+      ],
+      [
+        'id' => 3,
+        'title' => 'Manager',
+        'salary' => '$70.000',
+      ],
+    ]
+  ]);
+});
+
+Route::get('/job/{id}', function ($id) {
+  $jobs = [
+    [
+      'id' => 1,
+      'title' => 'Programmer',
+      'salary' => '$10.000',
+    ],
+    [
+      'id' => 2,
+      'title' => 'Designer',
+      'salary' => '$30.000',
+    ],
+    [
+      'id' => 3,
+      'title' => 'Manager',
+      'salary' => '$70.000',
+    ],
+  ];
+  $job = Arr::first($jobs, fn($job) => $job['id'] == $id );
+  return view('job', [
+    'job' => $job,
+  ]);
 });
 
 Route::get('/contact', function () {
