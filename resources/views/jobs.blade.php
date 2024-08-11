ef0631911ea654c28dfbf5a9f5033579b2ece575
Abdelali Laaguidi <abdelali.laaguidi.17@gmail.com>
2024-08-11 12:43:49 +0100

    Relaying on Eager Loading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -6,7 +6,9 @@
     @foreach ($jobs as $job)
       <li>
         <a href="/job/{{$job['id']}}" class="block px-8 py-4 mb-4 border border-gray-300 rounded-lg hover:bg-gray-200">
-          <div></div>
+          <div class="font-bold text-blue-500">
+            {{$job->employer->name}}
+          </div>
           <div>
             <strong>{{$job['title']}}</strong>: Pays {{$job['salary']}}
           </div>
