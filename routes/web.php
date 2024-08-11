539532f9273d6cd99dc3b461c7f2a15f8d4a0718
Abdelali Laaguidi <abdelali.laaguidi.17@gmail.com>
2024-08-11 21:42:57 +0100

    Adding Create a job with CSRF Protection

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,26 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-  return view('jobs', [
-    'jobs' => Job::with('employer')->paginate(3),
+  return view('job.index', [
+    'jobs' => Job::with('employer')->latest()->paginate(3),
   ]);
 });
 
+Route::post('/jobs', function () {
+  Job::create([
+    'title' => request('title'),
+    'salary' => request('salary'),
+    'employer_id' => 1,
+  ]);
+  return redirect('/jobs');
+});
+
+Route::get('/job/create', function () {
+  return view('job.create');
+});
+
 Route::get('/job/{id}', function ($id) {
-  return view('job', [
+  return view('job.show', [
     'job' => Job::find($id),
   ]);
 });
