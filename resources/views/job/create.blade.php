5d90f197d7de86b4e887b392d3c05a8a5e18f229
Abdelali Laaguidi <abdelali.laaguidi.17@gmail.com>
2024-08-11 22:12:37 +0100

    Adding Validation to Job Create Form

--- resources/views/job/create.blade.php
+++ resources/views/job/create.blade.php
@@ -16,16 +16,22 @@
             <div class="mt-2">
               <input type="text" name="title" id="title"
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="Senior Laravel Developer">
+                placeholder="Senior Laravel Developer" required>
             </div>
+            @error('title')
+              <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+            @enderror
           </div>
           <div class="sm:col-span-3">
             <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
             <div class="mt-2">
               <input type="text" name="salary" id="salary"
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="$120,000">
+                placeholder="$120,000" required>
             </div>
+            @error('salary')
+              <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+            @enderror
           </div>
         </div>
 
