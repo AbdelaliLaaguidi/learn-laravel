6aae817b5324f30a746ff9112ea46998cd0cddcb
Abdelali Laaguidi <abdelali.laaguidi.17@gmail.com>
2024-08-11 12:36:57 +0100

    updating jobs ui

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,8 +5,11 @@
   <ul>
     @foreach ($jobs as $job)
       <li>
-        <a href="/job/{{$job['id']}}" class="text-blue-500 hover:underline">
-          <strong>{{$job['title']}}</strong>: Pays {{$job['salary']}}
+        <a href="/job/{{$job['id']}}" class="block px-8 py-4 mb-4 border border-gray-300 rounded-lg hover:bg-gray-200">
+          <div></div>
+          <div>
+            <strong>{{$job['title']}}</strong>: Pays {{$job['salary']}}
+          </div>
         </a>
       </li>
     @endforeach
